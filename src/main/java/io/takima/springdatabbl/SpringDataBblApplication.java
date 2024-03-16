61cd6742a9a9e5a21857afa5420828a291549fa3
Jean-Pacome DELMAS <jean-pacome.delmas@external.totalenergies.com>
2024-03-16 16:45:11 +0100

    init project

---
+++ src/main/java/io/takima/springdatabbl/SpringDataBblApplication.java
@@ -0,0 +1,13 @@
+package io.takima.springdatabbl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataBblApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataBblApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/takima/springdatabbl/SpringDataBblApplicationTests.java
@@ -0,0 +1,13 @@
+package io.takima.springdatabbl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataBblApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
