db859438bc7dac1ad43338a1e533931ed95791ae
valentin-dumas <vdumas@takima.fr>
2024-05-12 20:55:04 +0200

    added custom ehcache caching config (commented out for demo purpose)

--- src/main/java/io/takima/springdatabbl/config/CachingConfig.java
+++ src/main/java/io/takima/springdatabbl/config/CachingConfig.java
@@ -7,4 +7,20 @@ import org.springframework.context.annotation.Configuration;
 @EnableCaching
 public class CachingConfig {
 
+    // Custom cache manager configuration (useful for EhCache)
+
+//    @Bean
+//    public CacheManager cacheManager() {
+//        CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+//                .withCache("products",
+//                        CacheConfigurationBuilder.newCacheConfigurationBuilder(
+//                                        Long.class, Object.class,
+//                                        ResourcePoolsBuilder.heap(1000)) // Maximum number of entries in cache
+//                                .withExpiry(
+//                                        Duration.of(10, TimeUnit.MINUTES) // Time-to-live for cache entries
+//                                )
+//                )
+//                .build(true);
+//        return cacheManager;
+//    }
 }
